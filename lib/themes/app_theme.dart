3a07bd6a2b1fc58da1a8be556136096c0cd0f8f3
Saubhik Singh <saubhik.singh01@gmail.com>
2021-01-26 19:12:40 +0530

    Fixes sonar

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -3,18 +3,17 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static Color bgColor = Color(0xFF2C2D31);
   static ThemeData darkTheme = ThemeData(
-    scaffoldBackgroundColor: bgColor,
-    appBarTheme: AppBarTheme(
-      color: bgColor,
-      elevation: 0,
-    ),
-    textTheme: TextTheme(
-        bodyText1: TextStyle(color: Colors.white),
-        headline1: TextStyle(color: Colors.white)),
-    visualDensity: VisualDensity.adaptivePlatformDensity,
-    cursorColor: Colors.white70,
-    hintColor: Colors.white60
-  );
+      scaffoldBackgroundColor: bgColor,
+      appBarTheme: AppBarTheme(
+        color: bgColor,
+        elevation: 0,
+      ),
+      textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.white),
+          headline1: TextStyle(color: Colors.white)),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      cursorColor: Colors.white70,
+      hintColor: Colors.white60);
 
   static AppBar customAppBar = AppBar(
     backgroundColor: bgColor,
