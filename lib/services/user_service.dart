9e749f68a425e93726451b445ebc2f172ce618a9
Saubhik Singh <saubhik.singh01@gmail.com>
2021-03-13 18:40:52 +0530

    Adds code to encrypt master password

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,14 +1,17 @@
 import 'package:cryptokeep/utils/app_snackbar.dart';
 import 'package:cryptokeep/models/user_model.dart';
 import 'package:cryptokeep/utils/constants.dart';
+import 'package:cryptokeep/utils/encryption.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
 class UserRepository {
   BuildContext _context;
+  EncryptionService _service;
 
   UserRepository._privateConstructor(BuildContext context) {
     this._context = context;
+    this._service = EncryptionService();
   }
 
   static UserRepository instance(BuildContext context) {
@@ -17,13 +20,17 @@ class UserRepository {
 
   void saveUser(User user) {
     var box = Hive.box<dynamic>(USER_BOX);
+    user.masterPassword = this._service.encrypt(user.masterPassword);
     box.put(IS_USER, user.toMap());
     Navigator.of(_context).pushReplacementNamed("/home");
   }
 
   static void signIn(BuildContext context, String masterPassword) async {
     final response = Hive.box<dynamic>(USER_BOX).get(IS_USER);
-    final isAuth = response["masterPassword"] == masterPassword;
+    String userMasterPassword =
+        EncryptionService().decrypt(response["masterPassword"]);
+
+    final isAuth = userMasterPassword == masterPassword;
 
     if (!isAuth) {
       AppSnackBar.show(context, text: "Auth Failed");
