9bd197a73dbedfd345add834a0cdc649080393c0
Saubhik Singh <saubhik.singh01@gmail.com>
2021-03-24 20:36:20 +0530

    Makes changes for settings page

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,43 @@
 import 'dart:io';
 
-import 'package:cryptokeep/provider/login_provider.dart';
+import 'package:cryptokeep/controller/app_controller.dart';
 import 'package:cryptokeep/routes.dart';
 import 'package:cryptokeep/themes/app_theme.dart';
 import 'package:cryptokeep/utils/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:provider/provider.dart';
-
-const kChannel = "com.singhsaubhik.cryptokeep/screencatpure";
-
-void setSystemSettings() {
-  final settings = SystemUiOverlayStyle(
-    systemNavigationBarColor: kScaffoldBackgroundColor, // navigation bar color,
-    systemNavigationBarIconBrightness: Brightness.dark, //navigation bar icon
-  );
-  SystemChrome.setSystemUIOverlayStyle(settings);
-}
-
-Future<dynamic> setConfigs() async {
-  /// TODO: Apply disableCaptureScreen on the basis of config
-  const MethodChannel _channel = const MethodChannel(kChannel);
-  return await _channel.invokeMethod("disableScreenCapture");
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setSystemSettings();
 
   Directory _directory = await getApplicationDocumentsDirectory();
   Hive.init(_directory.path);
   final box = await Hive.openBox(USER_BOX);
+  await Hive.openBox(SETTINGS_CONFIG_BOX);
   final isUser = box.get(IS_USER, defaultValue: null);
-  await setConfigs();
   runApp(MyApp(isUser));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends GetView<AppController> {
   // This widget is the root of your application.
   final isUser;
+  final controller = Get.put(AppController());
 
   MyApp(this.isUser);
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => PasswordProvider(),
-      child: MaterialApp(
-        title: 'CryptoKeep',
-        theme: AppTheme.darkTheme,
-        themeMode: ThemeMode.system,
-        routes: appRoutes,
-        initialRoute: isUser == null ? "/get_started" : "/splash",
-      ),
+    return GetMaterialApp(
+      title: 'CryptoKeep',
+      theme: ThemeData.light(),
+      darkTheme: AppTheme.darkTheme,
+      themeMode:
+          controller.isDarkTheme.value ? ThemeMode.dark : ThemeMode.light,
+      routes: appRoutes,
+      initialRoute: isUser == null ? "/get_started" : "/splash",
+      // initialRoute: "/home",
     );
   }
 }
