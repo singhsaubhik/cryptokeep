6f58d1083d2d7cc175353525e918b3d56bcb9d37
Saubhik Singh <saubhik.singh01@gmail.com>
2021-03-29 16:26:37 +0530

    Segregate code for screen capture module

--- lib/utils/common.dart
+++ lib/utils/common.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'constants.dart';
 
 enum AuthType { fingerprint, password }
 enum LoginAction { create, update }
@@ -11,4 +16,14 @@ void unfocusAndNavigate(BuildContext context, String route,
   }
 
   Navigator.of(context).pushNamed(route, arguments: arguments);
+}
+
+Future setScreenCapture(bool val) async {
+  const MethodChannel _channel = const MethodChannel(CHANNEL);
+  if (!Platform.isAndroid) return;
+
+  if (val) {
+    return await _channel.invokeMethod("disableScreenCapture");
+  }
+  return await _channel.invokeMethod("enableScreenCapture");
 }
