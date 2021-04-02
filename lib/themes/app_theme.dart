c5ac7813728215258ca190a6f5d190fffbb3739d
Saubhik Singh <saubhik.singh01@gmail.com>
2021-04-02 12:15:41 +0530

    centarise dialog theme

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -3,35 +3,38 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static Color bgColor = Color(0xFF2C2D31);
-  static final accentColor = Colors.blue.shade200;
+  static final accentColor = Colors.blueAccent;
 
   static ThemeData darkTheme = ThemeData.dark().copyWith(
     primaryColor: kPrimaryColor,
     accentColor: accentColor,
     backgroundColor: kInactiveCardColour,
-    floatingActionButtonTheme:
-        FloatingActionButtonThemeData(backgroundColor: kAccentColor),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: kAccentColor,
+    ),
     scaffoldBackgroundColor: kScaffoldBackgroundColor,
     appBarTheme: AppBarTheme(
       color: kInactiveCardColour,
       elevation: 0,
     ),
     cardColor: kInactiveCardColour,
+    dialogTheme: DialogTheme(
+      backgroundColor: kInactiveCardColour,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white,
+    ),
   );
 
   static ThemeData lightTheme = ThemeData.light().copyWith(
-      primaryColor: Colors.blue,
-      accentColor: Colors.blue,
-      scaffoldBackgroundColor: Colors.grey.shade300,
-      appBarTheme: AppBarTheme(
-        elevation: 0,
-        backgroundColor: Colors.blue,
-      ),
-      cardColor: Colors.blue.shade300);
-
-  static AppBar customAppBar = AppBar(
-    backgroundColor: bgColor,
-    elevation: 0,
+    primaryColor: Colors.blue,
+    accentColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.grey.shade300,
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      backgroundColor: Colors.blue,
+    ),
+    cardColor: Colors.blue.shade300,
   );
 
   static InputDecoration textFieldDecoration = InputDecoration(
